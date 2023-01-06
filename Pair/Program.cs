803b842d2dca0f87b21450cd5c65de4e827cd2a6
romvasko <romvasko@yandex.by>
2023-01-06 08:57:50 +0300

    event pair upd1

--- Pair/Program.cs
+++ Pair/Program.cs
@@ -16,8 +16,7 @@ var clothes6 = new T_shirt() { Cost = "9500", Size = 28 };
 var list = new Magazine();
 var mgView = new MagazineView();
 
-ConsoleDelegate consoleDelegate = (string msg) => {Console.WriteLine(msg);};
-
+Action<string> consoleDelegate = (x) => Console.WriteLine(x);
 
 list.AddClothes(clothes1, consoleDelegate);
 list.AddClothes(clothes2, consoleDelegate); 
@@ -34,12 +33,11 @@ list.GetPopular(consoleDelegate);
 
 list.GetAllClothes(consoleDelegate);
 
-public delegate void ConsoleDelegate(string msg);
 public class Magazine
 {
     List<Clothes> clothes = new List<Clothes>();
-    public event ConsoleDelegate OnStorageFull;
-    public int AddClothes(Clothes clothes, ConsoleDelegate output)
+    public event Action<string> OnStorageFull;
+    public int AddClothes(Clothes clothes, Action<string> output)
     {
         OnStorageFull = output;
         if (this.clothes.Count >= 2) {
@@ -50,20 +48,20 @@ public class Magazine
         output($"Clothe added: {clothes.GetInfo()}");
         return 0;
     }
-    public void DeleteClothes(Clothes clothes, ConsoleDelegate output)
+    public void DeleteClothes(Clothes clothes, Action<string> output)
     {
         this.clothes.Remove(clothes);
         output($"Clothe deleted: {clothes.GetInfo()}");
     }
 
-    public void UpdateClothes(Clothes clothes, string newValue, ConsoleDelegate output)
+    public void UpdateClothes(Clothes clothes, string newValue, Action<string> output)
     {
         int i = this.clothes.IndexOf(clothes);
         this.clothes[i].UpdateCost(newValue);
         output($"Clothe updated: {this.clothes[i].GetInfo()}");
     }
 
-    public void GetPopular(ConsoleDelegate output)
+    public void GetPopular(Action<string> output)
     {
         int[] array = new int[this.clothes.Count];
         int i = 0;
@@ -75,7 +73,7 @@ public class Magazine
         var result = array.GroupBy(x => x).OrderByDescending(x => x.Count()).FirstOrDefault().Key;
         output($"Most popular size : {result}\n");
     }
-    public void GetAllClothes(ConsoleDelegate output)
+    public void GetAllClothes(Action<string> output)
     {
         StringBuilder sb = new StringBuilder();
         sb.Append("List of all clothes:\n");
